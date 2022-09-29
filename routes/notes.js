1ba3874f6ea5e07f90fdf6ea6bd68f8d2675668c
Brian Campbell <brian.campbell003@gmail.com>
2022-09-28 22:55:39 -0500

    Added delete route

--- routes/notes.js
+++ routes/notes.js
@@ -10,7 +10,7 @@ notes.get('/', (req, res) =>
   })
 );
 
-// POST Route for submitting feedback
+// POST Route for saving new note
 notes.post('/', (req, res) => {
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
@@ -24,10 +24,6 @@ notes.post('/', (req, res) => {
       id: uuidv4(),
     };
 
-    // let oldNotes = require("../db/db.json");
-    // oldNotes.push(newNote);
-    // const oldNotesString = JSON.stringify(oldNotes, null, 4);
-    
     fs.readFile('./db/db.json', (err, data) => {
       if (err) throw err;
       let oldNotes = JSON.parse(data);
@@ -35,7 +31,7 @@ notes.post('/', (req, res) => {
       const oldNotesString = JSON.stringify(oldNotes, null, 4);
       updatedNotes(oldNotesString);
     })
-    
+
     function updatedNotes(oldNotesString) {
       fs.writeFile('./db/db.json', oldNotesString, (err) => {
         if (err) {
@@ -55,6 +51,49 @@ notes.post('/', (req, res) => {
     res.json(response);
   } else {
     res.json('Error in posting note');
+  }
+});
+
+// DELETE Route for deleting a note
+notes.delete('/:id', (req, res) => {
+  // Variable assignment to hold id value
+  let id = req.params.id;
+
+  if (id) {
+    // Read database and remove selected entry from the array
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      let allNotes = JSON.parse(data);
+      for (let i = 0; i < allNotes.length; i++) {
+        if (allNotes[i].id === id) {
+          allNotes.splice(i, 1);
+        }
+      }
+      // Stringify new array and pass to updatedNotes function
+      const updatedString = JSON.stringify(allNotes, null, 4);
+      updatedNotes(updatedString);
+    })
+
+    // Write file for updated notes following deletion
+    function updatedNotes(updatedString) {
+      fs.writeFile('./db/db.json', updatedString, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log('\nFile Contents of file after append:',
+            fs.readFileSync('./db/db.json', 'utf8'));
+        }
+      });
+    }
+
+    const response = {
+      status: 'success'
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in deleting note');
   }
 });
 
--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Middleware for parsing JSON and urlencoded form data
+// Middleware for parsing JSON and urlencoded form data, router
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.use(express.static('public'));
